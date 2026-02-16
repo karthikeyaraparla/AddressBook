b8e0ddc4f2494f0246627896d39d061d348e6690
Raparla Karthikeya <karthikeyar1811@gmail.com>
2026-02-16 10:43:27 +0530

    UC2 is added

--- AddressBook/Core/AddressBookService.cs
+++ AddressBook/Core/AddressBookService.cs
@@ -1,1 +1,21 @@
 
+using System.Collections.Generic;
+using AddressBook.Models;
+
+namespace AddressBook.Core
+{
+    public class AddressBookService
+    {
+        private List<Person> persons = new List<Person>();
+
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            return persons;
+        }
+    }
+}
